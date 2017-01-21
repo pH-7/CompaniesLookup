c5df606072cd086afc7005b12796d7145d6d2bf5
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-01-21 19:17:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# CompaniesLookup
+
+Simple and fast tool to find companies (from their *Company Number* or *Company Name*).
+
+I realized there was no such console tools for finding a company from [Companies House](https://www.gov.uk/government/organisations/companies-house) database (similar to the *Unix whois command* for finding the domain names' details).
+
+![Find Companies with console interface](companies-lookup-example.png)
+
+
+## Steup
+
+* Be sure PHP 7.0 or higher is installed
+
+* Install Composer ([https://getcomposer.org])
+
+* Then run:
+
+```bash
+$ composer install
+```
+
+* Rename `.env.example` to `.env`
+
+```bash
+$ mv .env.example .env
+```
+
+* Request your Companies House API key => [https://developer.companieshouse.gov.uk/developer/applications]
+
+* Open the file (with your favourite text editor)
+
+```bash
+$ nano .env
+```
+
+* Add the Companies House API Key between the double quote `"` `"`
+
+* Change the permissions of the `companies` bash file (if you use the bash shortcut file)
+
+```bash
+$ chmod u+x ./companies
+```
+
+* Run the script!
+
+```bash
+$ ./companies <COMPANY_NAME / COMPANY_ID>
+```
+OR
+```bash
+php bin/companies companies:search <COMPANY_NAME / COMPANY_ID>
+```
+
+#### Example to find HSBC company
+
+With the Company Name:
+```bash
+$ ./companies HSBC
+```
+
+Or with the Company ID:
+```bash
+$ ./companies 06388542
+```
+
+* Enjoy!
+
+
+## Requirements
+
+* PHP 7.0 or higher
+* [Composer](https://getcomposer.org)
+* [Companies House API key](https://developer.companieshouse.gov.uk/developer/signin)
+
+
+## About me
+
+I'm **Pierre-Henry Soria**, Software Engineer (love that!) and also passionate about marketing and e-businesses.
+
+
+## Where to contact me?
+
+You can by email at **pierrehenrysoria+github [[AT]] gmail [[D0T]] com**
+
+
+## License
+
+Under [General Public License 3](http://www.gnu.org/licenses/gpl.html) or later.
---
+++ src/Search.php
@@ -0,0 +1,182 @@
+<?php
+/**
+* @author         Pierre-Henry Soria <pierrehenrysoria@gmail.com>
+* @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+* @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+namespace PH7\CompaniesLookup;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Netsensia\CompaniesHouse\Api\Client\Client;
+
+class Search extends Command
+{
+    private $sCompanyName;
+    private $iCompanyId;
+    private $oData;
+    private $oClient;
+
+    public function __construct(Client $oClient)
+    {
+        parent::__construct();
+        $this->oClient = $oClient;
+    }
+
+    /**
+     * Configure the arguments.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('companies:search')
+            ->setDescription('Search a company by its "name" or "company ID"')
+            ->addArgument(
+                'company',
+                InputArgument::REQUIRED
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->setCompanyArgument($input);
+
+        $this->setCompanyData($input, $output);
+
+        $this->showName($output);
+        $this->companyNumber($output);
+        $this->companyType($output);
+        $this->showCompanyStatus($output);
+        $this->showDateOfCreation($output);
+        $this->showAddress($output);
+    }
+
+    protected function showName(OutputInterface $output)
+    {
+        if (!empty($this->oData->title)) {
+            $sName =$this->oData->title;
+        } else {
+            $sName = $this->oData->company_name;
+        }
+
+        if (!empty($sName)) {
+            $output->writeln('<info>Full Company Name: ' . htmlspecialchars($sName) . '</info>');
+        } else {
+            $this->notFound('Company Name', $output);
+        }
+    }
+
+    protected function companyNumber(OutputInterface $output)
+    {
+        if (!empty($this->oData->company_number)) {
+            $output->writeln('<info>Company Number: ' . htmlspecialchars($this->oData->company_number) . '</info>');
+        } else {
+            $this->notFound('Company Number', $output);
+        }
+    }
+
+    protected function companyType(OutputInterface $output)
+    {
+        if (!empty($this->oData->company_type)) {
+            $sType = $this->oData->company_type;
+        } else {
+            $sType = $this->oData->type;
+        }
+
+        if (!empty($sType)) {
+            $output->writeln('<info>Company Type: ' . strtoupper(htmlspecialchars($sType)) . '</info>');
+        } else {
+            $this->notFound('Company Type', $output);
+        }
+    }
+
+    protected function showCompanyStatus(OutputInterface $output)
+    {
+        if (!empty($this->oData->company_status)) {
+            $output->writeln('<info>Company Status: ' . ucfirst(htmlspecialchars($this->oData->company_status)) . '</info>');
+        } else {
+            $this->notFound('Company Status', $output);
+        }
+    }
+
+    protected function showAddress(OutputInterface $output)
+    {
+        if (!empty($this->oData->registered_office_address)) {
+            $oAddress = $this->oData->registered_office_address;
+        } else {
+            $oAddress = $this->oData->address;
+        }
+
+        if (!empty($oAddress->address_line_1)) {
+            $output->writeln('<info>Address: ' . htmlspecialchars($oAddress->address_line_1) . '</info>');
+
+            if (!empty($oAddress->country)) {
+                $output->writeln('<info>Country: ' . htmlspecialchars($oAddress->country) . '</info>');
+            }
+            if (!empty($oAddress->postal_code)) {
+                $output->writeln('<info>Postal Code: ' . htmlspecialchars($oAddress->postal_code) . '</info>');
+            }
+            if (!empty($oAddress->locality)) {
+                $output->writeln('<info>Locality: ' . htmlspecialchars($oAddress->locality) . '</info>');
+            }
+        } else {
+            $this->notFound('Address', $output);
+        }
+    }
+
+    protected function showDateOfCreation(OutputInterface $output)
+    {
+        if (!empty($this->oData->date_of_creation)) {
+            $output->writeln('<info>Date Of Creation: ' . htmlspecialchars($this->oData->date_of_creation) . '</info>');
+        } else {
+            $this->notFound('Date Of Creation', $output);
+        }
+    }
+
+    protected function setCompanyData(InputInterface $input, OutputInterface $output)
+    {
+        if (!empty($this->iCompanyId)) {
+            $oProfile = $this->oClient->getCompanyProfile($this->iCompanyId);
+            if (!$oProfile) {
+                $this->displayCompanyNotFound($input, $output);
+                exit(1);
+            } else {
+                $this->oData = $oProfile;
+            }
+        } else {
+            $oProfile = $this->oClient->companySearch($this->sCompanyName);
+            if (!$oProfile || empty($oProfile->items[0])) {
+                $this->displayCompanyNotFound($input, $output);
+                exit(1);
+            } else {
+                $this->oData = $oProfile->items[0];
+            }
+        }
+    }
+
+    protected function displayCompanyNotFound(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<error>Company "' . $input->getArgument('company') . '" not found.</error>');
+    }
+
+    protected function notFound(string $sFieldName, OutputInterface $output)
+    {
+        $output->writeln('<error>"' . $sFieldName . '" not found.</error>');
+    }
+
+    protected function setCompanyArgument(InputInterface $input)
+    {
+        $mCompany = $input->getArgument('company');
+
+        if (is_numeric($mCompany) && strlen($mCompany) > 5) {
+            $this->iCompanyId = $mCompany;
+        } else {
+            $this->sCompanyName = $mCompany;
+        }
+    }
+}
