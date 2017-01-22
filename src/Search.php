c434ce80b28324d413c1dc36d14a546bd13e1e84
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-01-22 12:44:12 +0000

    Remove unused namespace

--- src/Search.php
+++ src/Search.php
@@ -10,7 +10,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Netsensia\CompaniesHouse\Api\Client\Client;
 
 class Search extends Command
